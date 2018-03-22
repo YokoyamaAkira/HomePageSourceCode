243ecba324a31e2df1b12e262ae78d24abec87ea
YokoyamaAkira <akr_factory@yahoo.co.jp>
2018-03-22 10:39:57 +0900

    初回
    ソースのみを管理（targetフォルダーを除外）

---
+++ src/main/java/com/washinSystems/homepage/WashinHomePageApplication.java
@@ -0,0 +1,12 @@
+package com.washinSystems.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashinHomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashinHomePageApplication.class, args);
+	}
+}
